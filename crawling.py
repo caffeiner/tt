c2e960392b3d83fa85328c6734140721a2ec9e7b
caffeiner <71819974+caffeiner@users.noreply.github.com>
2022-08-12 18:30:49 +0900

    modify

--- crawling.py
+++ crawling.py
@@ -4,9 +4,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 import os
+from selenium.webdriver.chrome.options import Options
+
 #crawling.spec file hidden imports >> 'selenium','selenium.webdriver.common.by','selenium.webdriver.common.keys'
-driver = webdriver.Chrome(executable_path="/home/ubuntu/jenkins-backend/BackEnd/crawling/tt/chromedriver")
-#
+#driver = webdriver.Chrome(executable_path="/home/ubuntu/jenkins-backend/BackEnd/crawling/tt/chromedriver")
+
+
+
+chrome_options = Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome("/home/ubuntu/jenkins-backend/BackEnd/crawling/tt/chromedriver",chrome_options=chrome_options)
+driver.get('https://www.google.nl/')
+
 driver.get("https://burst.shopify.com/")
 elem = driver.find_element(By.CSS_SELECTOR, "#search_search")
 elem.send_keys("cat")
