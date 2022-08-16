f2732a16919fb2ab0b7ab2d9582dfbd5fd68f058
caffeiner <71819974+caffeiner@users.noreply.github.com>
2022-08-16 17:28:55 +0900

    modify crawling

--- crawling.py
+++ crawling.py
@@ -9,7 +9,7 @@ from selenium.webdriver.chrome.options import Options
 
 #for use pyinstaller, you need to add in crawling.spec file, hidden imports >> 'selenium','selenium.webdriver.common.by','selenium.webdriver.common.keys'
 chrome_options = Options()
-chrome_options.add_argument('--headless')
+chrome_options.add_argument('--headless') #크롬창을 켜지 않고 백그라운드에서 크롤링
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--single-process')
 chrome_options.add_argument('--disable-dev-shm-usage')
@@ -39,6 +39,8 @@ conn = pymysql.connect(host='i7a707.p.ssafy.io',
 def crawlImage():
     images = driver.find_elements(By.CSS_SELECTOR, "#Main > section:nth-child(1) > div.grid.gutter-bottom > div > div > div > div.photo-tile > button")
 
+    print("다운로드 할 사진 갯수")
+    print(len(images))
     for i in range(len(images)):
         try:
             images[i].click()
@@ -50,16 +52,21 @@ def crawlImage():
                                           "#Main > section:nth-child(1) > div.grid.gutter-bottom > div > div:nth-child("+str(i+1)+") > div > div.photo-tile > a > div > img")
         imageClick.click()
 
-        #crawlTags()
+        #crawlTags
         tags = driver.find_elements(By.CLASS_NAME, "nowrap")
         tagSql = ""
         for tag in tags:
             tagSql += tag.get_attribute('innerHTML') +","
         tagSql = tagSql[:-1] #맨 마지막 콤마 제거
+        print("---------tagSql---------")
+        print(tagSql)
         driver.back() #뒤로가기
 
-        list_of_files = glob.glob('/var/www/html/S07P12A707/BackEnd/src/main/resources/static/img/*')
+        list_of_files = glob.glob('//var/www/html/crawling/img/*')
+        print("현재 폴더 내에 있는 파일들 중에")
+        print(list_of_files)
         latest_file = max(list_of_files, key=os.path.getctime)
+        print("방금 다운로드 받은 파일 경로")
         print(latest_file)
 
         #insert into user (id, password, email, recommendFlag, salt) values ('manager','a707!1402','kmj9247@naver.com', 1, 'salt');
@@ -69,6 +76,7 @@ def crawlImage():
 
         cur = conn.cursor()
         cur.execute(sql, (latest_file, tagSql, 1, 'manager'))
+        print("execute 끝, 한사이클 끝")
     conn.commit()
 
 crawlImage()
